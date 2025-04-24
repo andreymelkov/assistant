5cfd5b965922a224b17dc996239b0009ab34a6ed
voklem <andrey.melkov@gmail.com>
2025-04-24 10:30:42 +0300

    ollama llm

--- model_files/run_ollama.sh
+++ model_files/run_ollama.sh
@@ -5,5 +5,5 @@ ollama serve &  # Start Ollama in the background
 
 echo "Ollama is ready, creating the model..."
 
-ollama run qwen2.5-coder:7b
+ollama run qwen2.5-coder:14b
 # ollama create qwen2.5-coder-custom -f ./model_files/Modelfile
