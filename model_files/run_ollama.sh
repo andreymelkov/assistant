00cae24313947f86f9301ccdb8ef9085512049ec
Andrey Melkov <andrey.melkov@gmail.com>
2026-02-09 15:59:44 +0300

    Delete model_files directory

--- model_files/run_ollama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting Ollama server..."
-ollama serve &  # Start Ollama in the background
-
-echo "Ollama is ready, creating the model..."
-
-ollama run qwen2.5-coder:14b
-# ollama create qwen2.5-coder-custom -f ./model_files/Modelfile
